ae844ecb8d81ab0f6a2abf2a3d3a281600914808
Mitja Bezenšek <mitja.bezensek@istudio.si>
2014-08-13 21:20:35 +0200

    Initial commit

---
+++ FSM_Simple/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace FSM_Simple{
+   internal class Program{
+      private static void Main(string[] args){
+         var Q = new List<string>{"q0", "q1", "q2"};
+         var Sigma = new List<char>{'a'};
+         var Delta = new List<Transition>{
+            new Transition("q0", 'a', "q1"),
+            new Transition("q1", 'a', "q2"),
+            new Transition("q2", 'a', "q1")
+         };
+         var Q0 = "q0";
+         var F = new List<string>{"q0", "q2"};
+         var dFSM = new DeterministicFSM(Q, Sigma, Delta, Q0, F);
+
+         dFSM.Accepts("");
+         dFSM.Accepts("a");
+         dFSM.Accepts("aa");
+         dFSM.Accepts("aaa");
+         dFSM.Accepts("aaaa");
+         dFSM.Accepts("aaaaa");
+         dFSM.Accepts("aaaaaa");
+      }
+   }
+}
---
+++ FSM_Simple/Transition.cs
@@ -0,0 +1,18 @@
+﻿namespace FSM_Simple{
+   public class Transition{
+      public string StartState { get; private set; }
+      public char Symbol { get; private set; }
+      public string EndState { get; private set; }
+
+      public Transition(string startState, char symbol, string endState){
+         StartState = startState;
+         Symbol = symbol;
+         EndState = endState;
+      }
+
+      public override string ToString(){
+         return string.Format("({0}, {1}) -> {2}", StartState, Symbol, EndState);
+      }
+   }
+}
+
